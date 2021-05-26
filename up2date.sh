da200c511b366fb9e08eaa41d103f176b6a6b3ea
ahviplc <ahlc@sina.cn>
2021-05-26 17:08:21 +0800

    update up2date.sh.

--- up2date.sh
+++ up2date.sh
@@ -12,11 +12,12 @@ echo " ==== git log ====  "
 git log --pretty=format:"%h - %an, %ar : %s" -5;
 echo " ==== 开始停止docsify ====  "
 kill `cat docsify.pid`;
+echo "当前日期是 `date`" >> docsify.log
 echo " ==== 开始启动docsify 端口8099 ====  "
-nohup docsify serve --port 8099 > docsify.log 2>&1& echo $! > docsify.pid;
+nohup docsify serve --port 8099 >> docsify.log 2>&1& echo $! > docsify.pid;
 echo " ==== docsify.pid ==== "
 cat docsify.pid
 echo " ==== docsify.log ==== "
 cat docsify.log
+echo " ==== 扩展操作 你可以执行:【netstat -tunlp | grep `cat docsify.pid`】查其进程==== "
 echo " ==== 结束了 up2date ==== "
-echo " ==== 提示 你可以执行:【netstat -tunlp | grep `cat docsify.pid`】查其进程==== "
